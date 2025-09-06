4cf9444fbb69373f52ebde2c3c1e8aacd4a952d6
Abdulnasir Badamasi <abdulnasirbadamasi5@gmail.com>
2025-09-06 01:17:59 -0700

    .

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -9,10 +10,13 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.status(200).json("Hello, world!");
-});
+// mongoose.connect(process.env.MONGODB_URL as string)
 
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT);
+});
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json("Hello, world!");
 });
